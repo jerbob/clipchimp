ae2532bea6dffe17f30a837ce47d687108220bf9
Jeremiah Boby <mail@jeremiahboby.me>
2022-10-22 18:32:05 +0100

    Deprecate get_clip_config in favour of yt-dlp feature

--- backend/clipchimp/logic/__init__.py
+++ backend/clipchimp/logic/__init__.py
@@ -1,11 +1,22 @@
 import contextlib
 import functools
 import json
+import logging
+import os
 import re
 
-import youtube_dl
-from requests_html import AsyncHTMLSession
+import httpx
+import redis
+import yt_dlp
+from redis_lru import RedisLRU
 
+
+client = redis.StrictRedis(
+    host=os.getenv("REDIS_HOST", ""),
+    port=int(os.getenv("REDIS_PORT", 0)),
+)
+cache = RedisLRU(client)
+session = httpx.Client()
 
 CACHE_LIMIT: int = 100
 YTDL_OPTIONS = {
@@ -14,44 +25,38 @@ YTDL_OPTIONS = {
     "quiet": True,
     "prefer_ffmpeg": True,
 }
-CLIP_METADATA_PATTERN = r'\{"postId":"%s","startTimeMs":"(.*)","endTimeMs":"(.*)"\}'
+CLIP_METADATA_PATTERN = r'\{"postId":"%s","startTimeMs":"(.*?)","endTimeMs":"(.*?)"\}'
+
+logger = logging.getLogger("gunicorn.error")
 
 
-@functools.lru_cache(CACHE_LIMIT)
+@cache
 def is_supported(url: str) -> bool:
     """Check whether the provided URL can be downloaded from."""
-    for extractor in youtube_dl.extractor.gen_extractors():
+    for extractor in yt_dlp.extractor.gen_extractors():
         if extractor.suitable(url) and extractor.IE_NAME != "generic":
             return True
     return False
 
 
-@functools.lru_cache(CACHE_LIMIT)
+@cache
 def get_ytdl_metadata(url: str) -> dict:
     """Get all metadata from youtube-dl about the provided URL."""
-    with youtube_dl.YoutubeDL(YTDL_OPTIONS) as ytdl:
+    with yt_dlp.YoutubeDL(YTDL_OPTIONS) as ytdl:
         return ytdl.extract_info(url, download=False) or {}
 
 
-async def get_clip_config(
-    session: AsyncHTMLSession, clip_url: str, clip_id: str
-) -> dict:
+def get_clip_config(clip_url: str, clip_id: str) -> dict:
     """Get clip config metadata from the given clip page."""
-    response = session.get(clip_url)
-    page = response.content.decode()
+    clip_metadata = {}
 
+    response = session.get(clip_url, cookies={})
+    if consent_request := response.next_request:
+        del consent_request.headers["cookie"]
+        response = session.send(consent_request, follow_redirects=True)
+
+    page = response.content.decode()
     if match := re.search(CLIP_METADATA_PATTERN % clip_id, page):
         with contextlib.suppress(json.JSONDecodeError):
             clip_metadata = json.loads(match.group(0))
-
-    await response.html.arender()
-    clip_metadata = await response.html.arender(
-        script="""
-            () => {
-                document.querySelector("button").click();
-                return ytInitialPlayerResponse.clipConfig
-            }
-        """,
-        send_cookies_session=True,
-    )
     return clip_metadata
