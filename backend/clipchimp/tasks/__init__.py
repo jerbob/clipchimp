2de31d8a5a6755491b1fce8d9baaf2f7542070c3
Jeremiah Boby <mail@jeremiahboby.me>
2022-10-22 18:33:19 +0100

    Invoke yt-dlp directly to download segments

--- backend/clipchimp/tasks/__init__.py
+++ backend/clipchimp/tasks/__init__.py
@@ -1,12 +1,15 @@
+import logging
 import subprocess
 from datetime import timedelta
 from os import getenv
 
 from celery import Celery
 
+from clipchimp import logic
 from clipchimp.models import DownloadParameters
 
 
+CLIP_METADATA_PATTERN = r'\{"postId":"%s","startTimeMs":"(.*)","endTimeMs":"(.*)"\}'
 REDIS_HOST, REDIS_PORT = getenv("REDIS_HOST"), getenv("REDIS_PORT")
 
 app = Celery(
@@ -18,7 +21,14 @@ app = Celery(
 
 @app.task
 def download_segment(url: str, start: timedelta, end: timedelta) -> None:
-    params = DownloadParameters(clip_url=url, start=start, end=end)  # type: ignore
-    command = f"ffmpeg -ss {start} -i {params.metadata['url']} -to {end} -c copy /downloads/{params.id}.mp4"
-    print(f"Running the following command:\n{command}")
-    subprocess.run(command.split(" "))
+    params = DownloadParameters(url=url, start=start, end=end)  # type: ignore
+    command = [
+        "yt-dlp",
+        "--download-sections",
+        f"*{start}-{end}",
+        url,
+        "-o",
+        f"/downloads/{params.id}",
+    ]
+    logging.info(" ".join(command))
+    subprocess.run(command)
