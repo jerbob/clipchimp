8e08e327c6dac731608d1e2e62c821368484fa9f
Jeremiah Boby <mail@jeremiahboby.me>
2022-10-30 02:57:51 +0000

    Add element for download modal

--- frontend/components/home/home.tsx
+++ frontend/components/home/home.tsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from "https://npm.tfl.dev/react";
+import React, { useState } from "https://npm.tfl.dev/react";
 import Helmet from "https://npm.tfl.dev/react-helmet@6.1.0";
-import jumper from "https://tfl.dev/@truffle/utils@~0.0.2/jumper/jumper.ts";
 import { useStyleSheet } from "https://tfl.dev/@truffle/distribute@^2.0.5/format/wc/react/index.ts";
 
 import styleSheet from "./home.scss.js";
@@ -8,60 +7,71 @@ import styleSheet from "./home.scss.js";
 function ExtensionMapping() {
   useStyleSheet(styleSheet);
 
-  const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
-
-  useEffect(() => {
-    const style = {
-      width: "400px",
-      height: "400px",
-      background: "#fff",
-      position: "fixed",
-      bottom: 0,
-      "z-index": "999",
-    };
-    // set styles for this iframe within YouTube's site
-    jumper.call("layout.applyLayoutConfigSteps", {
-      layoutConfigSteps: [
-        { action: "useSubject" }, // start with our iframe
-        { action: "setStyle", value: style },
-      ],
-    });
-  }, []);
-
-  const [downloadButton, setDownloadButton] = useState({text: "Download clip"});
-  const [formData, setFormData] = useState({url: "", start: "", end: ""});
+  const [formData, setFormData] = useState({
+    url: {error: false, message: "", value: ""},
+    start: {error: false, message: "", value: ""},
+    end: {error: false, message: "", value: ""}
+  })
+  const [formStatus, setFormStatus] = useState({loading: false, download: ""});
 
   function handleStartUpdate(event) {
-    setFormData({url: formData.url, start: event.target.value, end: formData.end})
+    setFormData({
+      url: formData.url,
+      start: {
+        error: formData.start.error,
+        message: formData.start.message,
+        value: event.target.value
+      },
+      end: formData.end
+    })
   }
+
   function handleEndUpdate(event) {
-    setFormData({url: formData.url, start: formData.start, end: event.target.value})
+    setFormData({
+      url: formData.url,
+      start: formData.start,
+      end: {
+        error: formData.end.error,
+        message: formData.end.message,
+        value: event.target.value
+      }
+    })
   }
+
   function handleURLUpdate(event) {
-    setFormData({url: event.target.value})
+    setFormData({
+      url: {
+        error: formData.url.error,
+        message: formData.url.message,
+        value: event.target.value
+      },
+      start: formData.start,
+      end: formData.end
+    })
     fetch(
       "/api/validate?" + new URLSearchParams(
-        {url: event.target.value, start: formData.start, end: formData.end}
+        {url: event.target.value, start: formData.start.value, end: formData.end.value}
       )
     ).then((response) => response.json()).then(
       (response) => {
-        setFormData({url: response.url, start: response.start, end: response.end})
+        setFormData(response);
       }
-  )
+    )
   }
+
   function handleSubmit(event) {
-    setDownloadButton({text: "Loading..."});
     event.preventDefault();
     fetch(
       "/api/download?" + new URLSearchParams(
-        {url: formData.url, start: formData.start, end: formData.end}
+        {url: formData.url.value, start: formData.start.value, end: formData.end.value}
       )
     ).then((response) => response.json()).then((downloadTask) => {
       if (downloadTask.status == "PENDING" || downloadTask.status == "SUCCESS") {
-        window.downloadCompleted = false;
+        setFormStatus({loading: true, download: ""});
+        window.loading = true;
         setInterval(
           () => {
-            if (window.downloadCompleted) {
+            if (!window.loading) {
               return
             }
             fetch(
@@ -70,9 +80,8 @@ function ExtensionMapping() {
               )
             ).then((response) => response.json()).then((downloadStatus) => {
               if (downloadStatus.status == "SUCCESS") {
-                window.downloadCompleted = true;
-                setDownloadButton({text: "Download clip"});
-                window.location = downloadStatus.download
+                setFormStatus({loading: false, download: downloadStatus.download})
+                window.loading = false;
               }
             })
           }, 1000)
@@ -89,25 +98,49 @@ function ExtensionMapping() {
         <link href="https://fonts.googleapis.com/css2?family=Alfa+Slab+One&family=Inter:wght@400;500;600;800&display=swap" rel="stylesheet" />
       </Helmet>
 
+      <div id="myModal" class="modal" className={`modal${formStatus.download ? "": " hidden"}`}>
+        <div class="modal-content">
+          <a id="download-link" href={`${formStatus.download}`} download="clip.webm">Save clip</a>
+        </div>
+      </div>
+
       <img src="/clip_downloader_logo.svg" />
       <p class="header">Download Youtube <br/>video clips</p>
-      <div class="form-container">
+
+      <div class="form-container" className={`form-container${formStatus.loading ? " loading-form": ""}`}>
+        <div className={`loader${formStatus.loading ? "": " hidden"}`}></div>
         <p class="form-title">Create your clip</p>
         <form onSubmit={handleSubmit}>
           <label for="url">Youtube video or clip URL</label> <br />
-          <input type="text" id="url" name="url" placeholder="enter url" onChange={handleURLUpdate} value={formData.url} /> <br />
+          <input
+            type="text" id="url" name="url" placeholder="enter url"
+            onChange={handleURLUpdate} value={formData.url.value}
+            className={`${formData.url.error ? "error-field": ""}`}
+          />
+          <p class="error-message">{formData.url.message}</p>
+          <br />
 
           <div class="row">
             <div class="column">
               <label for="start">Start time</label> <br />
-              <input type="text" id="start" name="start" placeholder="0:00:00" onChange={handleStartUpdate} value={formData.start} />
+              <input
+                type="text" id="start" name="start" placeholder="0:00:00"
+                onChange={handleStartUpdate} value={formData.start.value}
+                className={`${formData.start.error ? "error-field": ""}`}
+              />
+              <p class="error-message">{formData.start.message}</p>
             </div>
             <div class="column">
               <label for="start">End time</label> <br />
-              <input type="text" id="end" name="end" placeholder="0:00:00" onChange={handleEndUpdate} value={formData.end} />
+              <input
+                type="text" id="end" name="end" placeholder="0:00:00"
+                onChange={handleEndUpdate} value={formData.end.value}
+                className={`${formData.end.error ? "error-field": ""}`}
+              />
+              <p class="error-message">{formData.end.message}</p>
             </div>
           </div>
-          <input type="submit" value={downloadButton.text} id="download" />
+          <input type="submit" value="Download clip" id="download" />
         </form>
       </div>
 
