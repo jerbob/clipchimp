226cd3a5f40e69304c336a18883ced125b269d2b
Jeremiah Boby <mail@jeremiahboby.me>
2022-12-13 02:37:16 +0000

    Bump package version for frontend

--- frontend/truffle.config.mjs
+++ frontend/truffle.config.mjs
@@ -1,6 +1,6 @@
 export default {
   name: "@early-access-jerbob/clipchimp",
-  version: "0.5.6",
+  version: "0.5.7",
   apiUrl: "https://mycelium.staging.bio/graphql",
   requestedPermissions: [],
   installActionRel: {},
