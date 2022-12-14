fab6ab253c47bad00a5546cae1d484e7cca99cd3
Jeremiah Boby <mail@jeremiahboby.me>
2022-12-14 02:53:21 +0000

    Set default CORS allow to wildcard

--- backend/clipchimp/config.py
+++ backend/clipchimp/config.py
@@ -6,12 +6,10 @@ from uvicorn.workers import UvicornWorker
 
 
 ZERO_SECONDS = "0:00:00"
+DEFAULT_ALLOWED_ORIGIN = "*"
 ALLOWED_ORIGIN = getenv("ALLOWED_ORIGIN")
 DELTA_PATTERN = re.compile(r"(\d+:)?(\d+:)?\d+")
 DOWNLOADS = Path(getenv("DOWNLOADS_DIRECTORY") or "/downloads/").absolute()
-DEFAULT_ALLOWED_ORIGIN = (
-    "https://package-version-ff3e16f0-7a8d-11ed-8737-951d64f4280f.sporocarp.dev"
-)
 
 
 class CustomWorker(UvicornWorker):
