c5bdde458f28a8ad405546d1da9bc7509198f8b5
Jeremiah Boby <mail@jeremiahboby.me>
2022-12-23 22:52:34 +0000

    Update package version

--- frontend/truffle.config.mjs
+++ frontend/truffle.config.mjs
@@ -1,6 +1,6 @@
 export default {
   name: "@jerbob/clipchimp",
-  version: "0.6.5",
+  version: "0.6.6",
   apiUrl: "https://mycelium.truffle.vip/graphql",
   requestedPermissions: [],
   installActionRel: {},
